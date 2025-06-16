bb35dd7257cbb0ef11a1e17a17619d65eb5d6005
William Goodspeed <goodspeed@anche.no>
2025-06-16 21:32:28 +0800

    Bump the minimal SDK version to 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "cn.rad1o.riglogger"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
